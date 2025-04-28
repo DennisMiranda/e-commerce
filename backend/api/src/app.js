74db759075a9dea5d740189d732d84aa9be44a61
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-04-28 16:08:58 -0600

    Add orders endopint and card

--- backend/api/src/app.js
+++ backend/api/src/app.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import categoriesRoutes from "./routes/categories.routes.js";
 import productRoutes from "./routes/products.routes.js";
+import ordersRoutes from "./routes/orders.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import { checkToken } from "./midleware/auth.midleware.js";
 
@@ -26,6 +27,7 @@ app.use(express.json());
 // Montar rutas desde archivo externo
 app.use(productRoutes);
 app.use(categoriesRoutes);
+app.use(ordersRoutes);
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
