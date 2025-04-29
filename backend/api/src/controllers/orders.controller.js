57a685e44c6540b718d9b67886801c5d1ee1f4b0
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-04-28 22:40:52 -0600

    Add button orders and endpoint

--- backend/api/src/controllers/orders.controller.js
+++ backend/api/src/controllers/orders.controller.js
@@ -1,25 +1,29 @@
 import { getConnection } from "../database/connection.js";
+import { decryptToken } from "./auth.controller.js";
 
 export const getOrders = async (req, res) => {
-  const dbConnection = await getConnection();
-  const result = await dbConnection.request().query(`SELECT 
-po.id, 
-po.create_date, 
-po.total,
-po.status,
-p.id as productId,
-pod.quantity,
-pod.sell_price,
-p.name,
-p.description,
-p.image
-FROM purchase_order as po 
-inner join purchase_order_details as pod
-ON po.id = pod.purchase_order_id
-inner join products as p
-ON pod.products_id =p.id`);
+  const token = req.headers["authorization"];
+  const userData = decryptToken(token);
 
-  console.log(result);
+  const dbConnection = await getConnection();
+  const result = await dbConnection.request().input("userId", userData.id)
+    .query(`SELECT 
+    po.id, 
+    po.create_date, 
+    po.total,
+    po.status,
+    p.id as productId,
+    pod.quantity,
+    pod.sell_price,
+    p.name,
+    p.description,
+    p.image
+    FROM purchase_order as po 
+    inner join purchase_order_details as pod
+    ON po.id = pod.purchase_order_id
+    inner join products as p
+    ON pod.products_id =p.id 
+    WHERE po.users_id=@userId`);
 
   res.send({ data: result.recordset });
 };
