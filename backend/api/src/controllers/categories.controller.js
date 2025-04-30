0224c522e5800b086568336aa7b5ce4010cc074c
Dennis Miranda <dennise.miranda.15@gmail.com>
2025-04-30 09:45:26 -0600

    delete pages

--- backend/api/src/controllers/categories.controller.js
+++ backend/api/src/controllers/categories.controller.js
@@ -10,20 +10,35 @@ export const getCategories = async (req, res) => {
   const result = await dbConnection.request().query(query);
 
   const categories = result.recordset.reduce((acc, row) => {
-    const category = acc.find(cat => cat.id === row.id);
+    const category = acc.find((cat) => cat.id === row.id);
     if (!category) {
       acc.push({
         id: row.id,
         name: row.name,
-        products: row.product_id ? [{ id: row.product_id, name: row.product_name, price: row.price, stock: row.stock }] : []
+        products: row.product_id
+          ? [
+              {
+                id: row.product_id,
+                name: row.product_name,
+                price: row.price,
+                stock: row.stock,
+              },
+            ]
+          : [],
       });
     } else {
       if (row.product_id) {
-        category.products.push({ id: row.product_id, name: row.product_name, price: row.price, stock: row.stock });
+        category.products.push({
+          id: row.product_id,
+          name: row.product_name,
+          price: row.price,
+          stock: row.stock,
+        });
       }
     }
     return acc;
   }, []);
 
   res.send(categories); // Enviar las categorías con productos
+  console.log(categories);
 };
